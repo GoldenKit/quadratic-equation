c3fc8b904a6128d6bea1313bf4ef95bfc894c919
GoldenKit <nikitoska01@gmail.com>
2018-09-23 20:57:04 +0300

    solution_quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+var string = equation.split(' ');
+var a = Number(string[0]);
+if (string[3] == "+")
+    var b = Number(string[4]);
+  else
+    var b = Number(string[4])*(-1);
+if (string[7] == "+")
+    var c = Number(string[8]);
+else
+    var c = Number(string[8])*(-1);
+
+var x1 = Math.round((-b + Math.sqrt((b * b) - (4*a*c))) / (2 * a));
+var x2 = Math.round((-b - Math.sqrt((b * b) - (4*a*c))) / (2 * a));
+
+var x1x2 = [];
+if (x1 < x2) {
+  x1x2.push(x1);
+  x1x2.push(x2);
+  }
+  else {
+    x1x2.push(x2);
+    x1x2.push(x1);
+  }
+return x1x2;
 }
